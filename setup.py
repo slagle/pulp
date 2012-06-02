5709acf755a24ad4e1bbd50c9fe5ff0e10c8cf12
James Slagle <jslagle@redhat.com>
2012-06-02 13:52:11 -0400

    config changes

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import os
 from setuptools import setup, find_packages
 
 requires = [
-    'web.py', 'pymongo', 'simplejson', 'isodate',
+    'web.py', 'pymongo', 'simplejson', 'python-isodate',
 ]
 
 setup(
