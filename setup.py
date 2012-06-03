1853b662f18d8dd41828e6403b9b6feec1007614
James Slagle <jslagle@redhat.com>
2012-06-03 17:18:01 -0400

    remove pypam dep

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import os
 from setuptools import setup, find_packages
 
 requires = [
-    'web.py', 'pymongo', 'simplejson', 'isodate', 'PyPAM',
+    'web.py', 'pymongo', 'simplejson', 'isodate', 
 ]
 
 setup(
